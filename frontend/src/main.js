f20d606b1e267613802bb9d5a1e6f2751b12ec3d
mwleinad <dlopez@trazzos.com>
2019-03-16 18:10:38 -0500

    this connects to the database

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import DashboardPlugin from './dashboard-plugin';
+import axios from 'axios';
 
 // Plugins
 import App from './App.vue';
@@ -12,15 +13,21 @@ import routes from './routes/routes';
 Vue.use(VueRouter);
 Vue.use(DashboardPlugin);
 
-// configure router
-const router = new VueRouter({
-  routes, // short for routes: routes
-  linkActiveClass: 'active'
-});
+axios.get('/api/testa')
+  .then((response) => {
+    console.log(response.data);
+    // configure router
+    const router = new VueRouter({
+      routes, // short for routes: routes
+      linkActiveClass: 'active'
+    });
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: h => h(App),
-  router
-});
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#app',
+      render: h => h(App),
+      router
+    });
+  });
+
+
