6b68733a4c1e4e443f0ad7f05e5db97e531746bc
mwleinad <dlopez@trazzos.com>
2019-09-08 14:04:53 -0500

    database

--- database/migrations/2019_09_08_000000_create_user_table.php
+++ database/migrations/2019_09_08_000000_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->increments('id');
+            $table->uuid('uuid');
             $table->string('name');
+            $table->integer('company_id')->unsigned();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('type')->default('manager');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user');
     }
 }
